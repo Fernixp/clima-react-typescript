c28d95856ecfea6464f1f560af3de05b9de3ca9f
Fernando Nina <Fernandonina611@gmail.com>
2025-11-26 21:58:02 -0400

    feat: :sparkles: instalando zod y testeando tipado de la api

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,17 @@
 import type { SearchType } from "../types";
 import axios from "axios";
+import { z } from "zod";
+
+const Weather = z.object({
+    name: z.string(),
+    main: z.object({
+        temp: z.number(),
+        temp_min: z.number(),
+        temp_max: z.number(),
+    })
+})
+
+type WeatherType = z.infer<typeof Weather>
 
 export default function useWeather() {
     /* usamos dotenv */
@@ -13,8 +25,19 @@ export default function useWeather() {
 
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`;
             
+            console.log(weatherUrl);
             const {data: weatherData} = await axios(weatherUrl);
-            console.log(weatherData);
+            //Zod
+            const result = Weather.safeParse(weatherData);
+            if (result.success) {
+                console.log(result.data.name);
+                console.log(result.data.main.temp);
+                console.log(result.data.main.temp_min);
+                console.log(result.data.main.temp_max);
+                return;
+            }else{
+                console.log('respuesta mal formada');
+            }
         } catch (error) {
             console.log('Error al obtener el clima: ' + error);
         }
