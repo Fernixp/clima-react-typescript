9c552ffcc57c1045553076a1ef726265656b24c5
Fernando Nina <Fernandonina611@gmail.com>
2025-12-27 13:21:05 -0400

    feat: :sparkles: agreegando spinner para feedback al usuario

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import styles from "./App.module.css";
 import Form from "./components/Form/Form";
 import WeatherDetail from "./components/WeatherDetail/WeatherDetail";
+import Spinner from "./components/Spinner/Spinner";
 import useWeather from "./hooks/useWeather";
 
 function App() {
-  const { weather, fetchWeather, hasWeatherData } = useWeather();
+  const { weather, fetchWeather, hasWeatherData, loading } = useWeather();
   return (
     <>
       <h1 className={styles.title}>Buscador de clima</h1>
 
       <div className={styles.container}>
         <Form fetchWeather={fetchWeather} />
-        {hasWeatherData ? (
+        {loading ? (
+          <Spinner />
+        ) : hasWeatherData ? (
           <WeatherDetail weather={weather} />
         ) : (
           <p>Ingrese datos de la ciudad para ver el clima</p>
